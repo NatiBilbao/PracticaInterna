866fa3d073554e1486ab13e8ed4c2b73106cd66b
NatyBilbao <natabilbaocano18@gmail.com>
2023-12-20 01:15:46 -0400

    Proyecto Base de Datos

---
+++ src/grouploans/grouploans.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Post, Body, Get, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
+import { CreateGroupLoansDto } from './dto/create-grouploans.dto';
+import { GrouploansService } from './grouploans.service';
+import { GroupLoan } from './grouploans.entity';
+import {UpdateGroupLoansDto } from './dto/update-grouploans.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Group Loan')
+@Controller('grouploan')
+export class GrouploansController {
+    constructor(private grouploanService: GrouploansService){}
+
+    @Get()
+    getGroupLoans(): Promise<GroupLoan[]>{
+        return this.grouploanService.getGroupLoans();
+    }
+
+    @Get(':id')
+    getGroupLoan(@Param('id', ParseIntPipe) id: number){
+        return this.grouploanService.getGroupLoan(id);
+    }
+
+    @Post()
+    createGroupLoan(@Body() newGroupLoan: CreateGroupLoansDto){
+       return this.grouploanService.createGroupLoan(newGroupLoan)
+    }
+
+    @Delete(':id')
+    deleteGroupLoan(@Param('id', ParseIntPipe) id: number){
+        return this.grouploanService.deleteGroupLoan(id)
+    }
+
+    @Patch(':id')
+    updateGroupLoan(@Param('id', ParseIntPipe) id:number, @Body() group: UpdateGroupLoansDto){
+        return this.grouploanService.updateMember(id, group)
+    } 
+
+}
+ 
